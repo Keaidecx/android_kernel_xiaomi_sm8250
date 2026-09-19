da6b23217b280e8addc36b0451ef695c94de8e48
KirinNova <3058098928@qq.com>
2026-09-19 14:45:39 +0800

    Update namei.c

--- fs/namei.c
+++ fs/namei.c
@@ -1682,19 +1682,21 @@ retry:
 		dentry = old;
 	}
 #ifdef CONFIG_KSU_SUSFS_SUS_PATH
-	if (unlikely(dentry) && !IS_ERR(dentry) && dentry->d_inode && !found_sus_path && susfs_is_inode_sus_path(dentry->d_inode)) {
+	if (unlikely(dentry) && !IS_ERR(dentry) && dentry->d_inode && !found_sus_path &&
+	    susfs_is_inode_sus_path(dentry->d_inode)) {
+
+		bool is_fuse = dentry->d_inode->i_sb->s_magic == FUSE_SUPER_MAGIC;
+
 		if (d_in_lookup(dentry))
 			d_lookup_done(dentry);
-		if (!(flags & LOOKUP_RCU))
-			dput(dentry);
-		// - Just in case if an user app has been granted full file access and
-		//   it is trying to find the fuse sus path with the create flag, then
-		//   at least we can prevent the fake qstr file from from being created,
-		//   although it is futile to do this, it is better than doing nothing.
-		if (dentry->d_inode->i_sb->s_magic == FUSE_SUPER_MAGIC &&
-			(flags & (LOOKUP_CREATE | LOOKUP_EXCL)))
-			return ERR_PTR(-EACCES);
-		dentry = d_alloc(base, &susfs_fake_qstr_name);
+		if (!(flags & LOOKUP_RCU)) {
+			//  it is trying to find the fuse sus path with the create flag, then
+			//  at least we can prevent the fake qstr file from from being created,
+			//  although it is futile to do this, it is better than doing nothing.
+			if (is_fuse && (flags & (LOOKUP_CREATE | LOOKUP_EXCL)))
+				return ERR_PTR(-EACCES);
+			dentry = d_alloc(base, &susfs_fake_qstr_name);
+		}
 		found_sus_path = true;
 		goto retry;
 	}
